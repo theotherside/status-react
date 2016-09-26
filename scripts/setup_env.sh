12530b7c8a2a7cf059c52b3ca85cd8d3673b5507
Jarrad Hope <me@jarradhope.com>
2016-09-26 17:21:28 +0800

    install sys-img-armeabi-v7a-android-23

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -18,6 +18,7 @@ echo 'export PATH=$ANDROID_HOME/tools:$PATH' >> ~/.bash_profile
 echo 'export PATH=$ANDROID_HOME/platform-tools:$PATH' >> ~/.bash_profile
 echo 'export PATH=$ANDROID_HOME/build-tools/$(ls $ANDROID_HOME/build-tools | sort | tail -1):$PATH' >> ~/.bash_profile
 ( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | android update sdk --no-https --no-ui
+echo "y" | android update sdk -a --no-ui --filter sys-img-armeabi-v7a-android-23
 . ~/.bash_profile
 brew outdated node || brew unlink node && brew install node
 brew outdated watchman || brew unlink watchman && brew install watchman
