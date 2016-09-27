5d649b7aa068482868828da9951c9e3b2ed95257
Jarrad Hope <me@jarradhope.com>
2016-09-27 13:48:19 +0800

    fix for appium?

--- scripts/appium_background.sh
+++ scripts/appium_background.sh
@@ -1,9 +1,14 @@
+#!/usr/bin/env bash
+# neither options work, lets go ghetto?
+appium &
+sleep 3
+
 #!/usr/bin/expect -f
-set timeout 600
-# cd [file dirname $argv0]
-exec cd ..
-spawn -ignore HUP appium
-expect -ex "Appium REST http interface listener started"
-send_user "Appium Initialized\n"
-expect_background
-exit 0
+# set timeout 600
+# # cd [file dirname $argv0]
+# exec cd ..
+# spawn -ignore HUP appium
+# expect -ex "Appium REST http interface listener started"
+# send_user "Appium Initialized\n"
+# expect_background
+# exit 0
