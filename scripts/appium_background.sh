2f9019ffa20f698cc339af0ae5d88b888ca80b45
Jarrad Hope <me@jarradhope.com>
2016-09-28 14:19:54 +0800

    jenkins pipeline

--- scripts/appium_background.sh
+++ scripts/appium_background.sh
@@ -2,13 +2,3 @@
 # neither options work, lets go ghetto?
 appium &
 sleep 3
-
-#!/usr/bin/expect -f
-# set timeout 600
-# # cd [file dirname $argv0]
-# exec cd ..
-# spawn -ignore HUP appium
-# expect -ex "Appium REST http interface listener started"
-# send_user "Appium Initialized\n"
-# expect_background
-# exit 0
